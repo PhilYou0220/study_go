a281a06d044ac8dcf17573eb79e1ea7eb1adaa22
PhilYou <1299639230@qq.com>
2023-03-20 01:07:06 +0800

    0320

---
+++ 10gin/10.5路由分组/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/gin-gonic/gin"
+import . "route/route"
+//服务端对客户端发起各种请求方式的处理
+
+
+//func book(ctx *gin.Context){
+//
+//	ctx.HTML(200,"login.html",nil)
+//}
+
+
+func main()	{
+	//获取引擎对象，即路由对象
+	r:=gin.Default()
+
+	//加载模板文件
+	//r.LoadHTMLGlob("templates/*")
+
+
+
+	//初始化路由
+	InitRoute(r)
+	//启动：默认本机8080端口
+	r.Run("127.0.0.1:8081")
+}
