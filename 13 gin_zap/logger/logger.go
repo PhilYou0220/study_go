4bd0409fd5662a68cdd5c0bcd4e1ee75f10a7814
PhilYou <1299639230@qq.com>
2023-06-06 20:35:42 +0800

    0606

--- 13 gin_zap/logger/logger.go
+++ 13 gin_zap/logger/logger.go
@@ -8,14 +8,17 @@ import (
 
 //var lg *zap.Logger //全局logger
 
-func Init(){
+func Init() error{
 	//1.encoder 编码器、格式器
 	encoderCfg := zap.NewProductionEncoderConfig()
 	encoderCfg.TimeKey = "time" //更改 {"ts":1684843048.9437547}
 	encoderCfg.EncodeTime =zapcore.ISO8601TimeEncoder //设置日期显示格式
 	encoder :=zapcore.NewJSONEncoder(encoderCfg) //zap.NewProductionEncoderConfig()一些json
 	//2、WriteSyncer（写到哪里）
-	file,_:=os.OpenFile("./test.log",os.O_CREATE | os.O_APPEND | os.O_WRONLY,0644)
+	file,err:=os.OpenFile("./test.log",os.O_CREATE | os.O_APPEND | os.O_WRONLY,0644)
+	if err !=nil{
+		return err
+	}
 	//传入文件句柄输出到文件里
 	fileWS := zapcore.AddSync(file)
 	consoleWS := zapcore.AddSync(os.Stdout)
@@ -24,7 +27,8 @@ func Init(){
 
 	lg := zap.New(core,zap.AddCaller())
 
-	//替换zap全局的logger
-	zap.ReplaceGlobals(lg)
+	//替换zap全局的logger zap.L() zap.L()时，实际上是获取了lg的快捷方式。
+	zap.ReplaceGlobals(lg) //这里之后就可以使用 zap.l()了
 	zap.L().Info("success")
+	return  nil //没报错
 }
--- 13 gin_zap/main.go
+++ 13 gin_zap/main.go
@@ -1,10 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin_zap/logger"
+	"gin_zap/middleware"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+
+/*https://www.liwenzhou.com/posts/Go/zap-in-gin/
+目的让zap记录gin本
+*/
 
 func main()  {
-	gin.SetMode(gin.ReleaseMode) //设置为生产模式
-	r := gin.Default() // 默认是debug模式
+	//初始化日志模块
+	err:=logger.Init()
+	if err!=nil{
+		panic(err)
+	}
+
+
+	gin.SetMode(gin.ReleaseMode) //设置为生产模式 不会显示一些debug日志
+	//r := gin.Default() // 默认是debug模式
+	r := gin.New() //自己写一个new函数
+	//注册两个自定义的中间件
+	r.Use(middleware.GinLogger(zap.L()),middleware.GinRecovery(zap.L(),false))
+	r.GET("/hello", func(c *gin.Context) {
+		zap.L().Info("req hello")
+		panic("123") //有提示但是不会报错
+	})
 
 	r.Run(":8001")
 
