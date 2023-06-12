0b565ba0fbd87d2da959219b17c731cef2a1f7ec
PhilYou <1299639230@qq.com>
2023-06-13 00:32:07 +0800

    0613

--- 9.接口与标准库/9.2 time包/main.go
+++ 9.接口与标准库/9.2 time包/main.go
@@ -44,39 +44,61 @@ func timestampDemo() {
 	nano := now.UnixNano()   // 纳秒时间戳
 	fmt.Println(timestamp, milli, micro, nano)
 }
-func main(){
+
+func timestampDemo2(timestamp int64) {
+	timeObj := time.Unix(timestamp, 0) //将时间戳转为时间格式
+
+	fmt.Println(timeObj)
+	year := timeObj.Year()     //年
+	month := timeObj.Month()   //月
+	day := timeObj.Day()       //日
+	hour := timeObj.Hour()     //小时
+	minute := timeObj.Minute() //分钟
+	second := timeObj.Second() //秒
+	fmt.Printf("%d-%02d-%02d %02d:%02d:%02d\n", year, month, day, hour, minute, second)
+}
+func main() {
 
 	//1获取当前时间 时间类型为 自定义类型 type Duration int64 单位是纳秒 1000*1000*1000是秒
-	now:=time.Now() //2023-06-09 20:27:59.0141575 +0800 CST m=+0.004994201
-	month:=time.Now().Month()// June
-	day:=time.Now().Day()//9
-	hour :=time.Now().Hour() //20
-	minute:=time.Now().Minute() //32
-	second := time.Now().Second()//32
-	fmt.Println(now,month,day,hour,minute,second)
+	now := time.Now()             //2023-06-09 20:27:59.0141575 +0800 CST m=+0.004994201
+	month := time.Now().Month()   // June
+	day := time.Now().Day()       //9
+	hour := time.Now().Hour()     //20
+	minute := time.Now().Minute() //32
+	second := time.Now().Second() //32
+	fmt.Println(now, month, day, hour, minute, second)
 
 	//2locatime和time zone
 	timezoneDemo()
 
 	//3 unixtime
 	timestampDemo() //时间戳都是UTC时间
-	//4 时间间隔
-	//add 增加时间
-	later := now.Add(time.Hour+time.Second*10) // 当前时间加1小时后的时间
+
+	//4 add 增加时间
+	later := now.Add(time.Hour + time.Second*10) // 当前时间加1小时后的时间
 	fmt.Println(later)
 
-	//Sub求两个时间之间的差值
+	//5 Sub求两个时间之间的差值
 	//func (t Time) Sub(u Time) Duration
-	duration := later.Sub(now) //later-now
-	fmt.Println(duration) //1h0m10s
-	fmt.Println(reflect.TypeOf(duration)) //time.Duration
-	var interval int64 = 3600*1000*1000*1000 //单位是纳秒 1000*1000*1000是秒
-	if duration>time.Duration(interval){
+	duration := later.Sub(now)                     //later-now
+	fmt.Println(duration)                          //1h0m10s
+	fmt.Println(reflect.TypeOf(duration))          //time.Duration
+	var interval int64 = 3600 * 1000 * 1000 * 1000 //单位是纳秒 1000*1000*1000是秒
+	if duration > time.Duration(interval) {
 		fmt.Println("这是真的")
-		fmt.Println(duration-time.Duration(interval)) //1h0m9.9999964s
+		fmt.Println(duration - time.Duration(interval)) //1h0m9.9999964s
 	}
 
-	//定时器选了chan再说
+	//6定时器选了chan再说
 
-	//
+	//7(时间格式转格式化字符串)
+	fmt.Println(now.Format("2006-01-02 15:04:05")) //2023-06-12 23:27:56
+
+	//时间戳转时间格式
+	timestampDemo2(1686587075)
+
+	//格式化字符串转时间格式
+	//time.ParseInLocation
+	//时间格式转时间戳
+	//fmt.Println(timeObj.Unix())
 }
